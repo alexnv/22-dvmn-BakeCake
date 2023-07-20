86c6658b0f9576f2642cdfed5eca3044f584b7fa
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-07-20 12:30:16 +0300

    changed urls routing

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
+from shop.models import Layer, Berries, Topping, Shape, Decor
+
 
 class IndexView(View):
     def get(self, request):
 
+        context = {
+            'layers':  Layer.objects.filter(available=True),
+            'shapes': Shape.objects.filter(available=True),
+            'toppings': Topping.objects.filter(available=True),
+            'berries': Berries.objects.filter(available=True),
+            'decors': Decor.objects.filter(available=True),
+        }
         context = {
 
         }
